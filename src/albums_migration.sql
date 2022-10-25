391e4b8726d89dbba525d4a20486bf000a1a9653
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-25 13:27:50 -0500

    fixed the code by replacing ; with a ,

--- src/albums_migration.sql
+++ src/albums_migration.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- auto-incrementing unsigned integer primary key
     artist VARCHAR(100), -- string for storing the recording artists name
     name  VARCHAR(100), -- string for storing a record name
-    release_date INT; -- integer representing year record was released
-    sales FLOAT; -- floating point value for number of records sold (in millions)
+    release_date INT, -- integer representing year record was released
+    sales FLOAT, -- floating point value for number of records sold (in millions)
     genre VARCHAR(100), -- string for storing the record's genre(s)
     PRIMARY KEY (id)
 );
