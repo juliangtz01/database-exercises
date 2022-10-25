774bcdbddee8784cef557402196fa429d6595f3f
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-25 15:25:26 -0500

    fixed the code for the albums_seeder.sql file

--- src/select_exercises.sql
+++ src/select_exercises.sql
@@ -1,1 +1,6 @@
 USE codeup_test_db;
+-- Write queries to find the following information.
+-- Before each item, output a caption explaining the results
+
+-- The name of all albums by Pink Floyd
+SELECT * name FROM albums WHERE artist = 'Pink Floyd';
