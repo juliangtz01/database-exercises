d8bef7ea5ec1a0f447db676cc19c89bfd7b15ce1
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-25 12:40:37 -0500

    created albums_migration.sql file and added sql code to it

---
+++ src/albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- auto-incrementing unsigned integer primary key
+    artist VARCHAR(100), -- string for storing the recording artists name
+    name  VARCHAR(100), -- string for storing a record name
+    release_date INT; -- integer representing year record was released
+    sales FLOAT; -- floating point value for number of records sold (in millions)
+    genre VARCHAR(100), -- string for storing the record's genre(s)
+    PRIMARY KEY (id)
+);
