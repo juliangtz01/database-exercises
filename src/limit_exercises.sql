1ffa893266a0dc38f16e7357d31b43dec0c54ec2
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-26 15:08:31 -0500

    completed the limit_exercises

---
+++ src/limit_exercises.sql
@@ -0,0 +1,7 @@
+USE employees;
+-- List the first 10 distinct last name sorted in descending order
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+-- get the top 5 salaries and display just the employees number from the salaries table
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
+-- Update the previous query to find the tenth page of results
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
