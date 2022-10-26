466228001e65f575055046cccb98ac2f0d402b4d
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-26 12:29:22 -0500

    working on the where_exercise.sql file

---
+++ src/where_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya');
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE gender = 'M'
