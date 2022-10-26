f1165444fd97b72607e85f8498b7c928516211a6
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-26 14:26:05 -0500

    completed the where_exercises.sql

--- src/where_exercises.sql
+++ src/where_exercises.sql
@@ -7,4 +7,11 @@ SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 -- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+-- Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
 SELECT * FROM employees WHERE gender = 'M' AND(first_name IN ('Irena', 'Vidya', 'Maya'));
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees WHERE last_name LIKE '%E' OR last_name like 'E%';
+-- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%E';
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
