813f6ae84ad454661356c2b0d765ee5b8a0b4487
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-27 10:45:32 -0500

    changed my code in the group by exercise

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -7,7 +7,7 @@ SELECT DISTINCT title FROM titles;
 SELECT last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY last_name ORDER BY last_name ASC;
 
 -- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows
-SELECT COUNT(*)  FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY last_name, first_name;
+SELECT last_name, first_name  FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY last_name, first_name;
 
 -- Find the unique last names with a 'q' but not 'qu'
 SELECT DISTINCT last_name FROM employees WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%';
